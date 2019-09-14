4a359fe106ae440934a6d573c15de01da398a6ed
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-09-14 18:19:03 +0800

    Use single output Stan code.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,10 +7,12 @@
 using namespace Rcpp;
 
 
-RcppExport SEXP _rcpp_module_boot_stan_fit4bc_without_pred_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4bc_with_pred_mod();
+RcppExport SEXP _rcpp_module_boot_stan_fit4bc_without_pred_multi_mod();
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_rcpp_module_boot_stan_fit4bc_without_pred_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4bc_without_pred_mod, 0},
+    {"_rcpp_module_boot_stan_fit4bc_with_pred_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4bc_with_pred_mod, 0},
+    {"_rcpp_module_boot_stan_fit4bc_without_pred_multi_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4bc_without_pred_multi_mod, 0},
     {NULL, NULL, 0}
 };
 
