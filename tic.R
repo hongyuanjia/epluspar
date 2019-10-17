191201b7f31cd9caa4d011671b611cc363896996
Hongyuan Jia <hongyuan.jia@bears-berkeley.sg>
2019-10-17 18:11:10 +0800

    Use tic for setting up CI.

---
+++ tic.R
@@ -0,0 +1,28 @@
+# add Makevars
+get_stage("before_install") %>%
+    add_code_step(system("mkdir -p ~/.R/")) %>%
+    add_code_step(writeLines(
+        "
+        CXX14 = g++-7 -fPIC -flto=2
+        CXX14FLAGS = -mtune=native -march=native -Wno-unused-variable -Wno-unused-function -Wno-unused-local-typedefs -Wno-ignored-attributes -Wno-deprecated-declarations -Wno-attributes -O3
+        ",
+        con = "~/.R/Makevars"
+    ))
+
+# install EnergyPlus v8.8
+get_stage("script") %>% 
+    add_code_step(eplusr::install_eplus(8.8))
+
+# R CMD Check
+build_args <- c("--force")
+do_package_checks(args = args, build_args = build_args)
+
+# pkgdown
+# make sure to clean site to rebuild everything
+if (ci_get_branch() == "master" && Sys.getenv("TRAVIS_OS_NAME") == "linux" && Sys.getenv("TRAVIS_R_VERSION_STRING") == "release") {
+    do_pkgdown(document = TRUE, orphan = TRUE)
+}
+
+# codecov
+get_stage("deploy") %>%
+  add_code_step(covr::codecov())
